bdd929d688e8e2ee884ed42e000260303251c967
laomatt <laomatt1@gmail.com>
2015-02-07 18:08:36 -0800

    site redesign

---
+++ main_code.js
@@ -0,0 +1,14 @@
+function display_blogs()
+{
+
+
+
+
+document.getElementById("blogcontent").innerHTML=" <h1>My DBC blogs</h1>  <p><h2>Technical blog entries</h2></p>        <div class=\"app_menu_item\">        <a href=\"t7-JavaScript.html\">Javascript vs. Ruby</a>        <div class=\"date\"> Febuary 7th, 2015</div>      </div>      <div class=\"app_menu_item\">        <a href=\"t6-oop-concepts.html\">Object orientated concepts</a>        <div class=\"date\"> January 29th, 2015</div>      </div>      <div class=\"app_menu_item\">        <a href=\"t5-ruby-classes.html\">Ruby classes</a>        <div class=\"date\"> January 17th, 2015</div>      </div>      <div class=\"app_menu_item\">        <a href=\"t4-enumerable-methods.html\">Enumerable methods</a>        <div class=\"date\"> January 2nd, 2015</div>      </div>        <div class=\"app_menu_item\">        <a href=\"t3-arrays-hashes.html\">Arrays vs. Hashes</a>        <div class=\"date\"> December 22nd, 2014</div>      </div>      <div class=\"app_menu_item\">        <a href=\"t2-css-design.html\">CSS design</a>        <div class=\"date\"> December 17th, 2014</div>      </div>      <div class=\"app_menu_item\">        <a href=\"t1-git-blog.html\">Git and Github</a>        <div class=\"date\"> December 10th, 2014</div>      </div>  <p><h2>Cultural blog entries</h2></p>      <div class=\"app_menu_item\">        <a href=\"c7-values.html\">Values</a>        <div class=\"date\"> Febuary 7th, 2015</div>      </div>      <div class=\"app_menu_item\">        <a href=\"c6-stereotype-threat.html\">Stereotype threat</a>        <div class=\"date\"> January 29th, 2015</div>      </div>      <div class=\"app_menu_item\">        <a href=\"c5-feedback.html\">Feedback cycle</a>        <div class=\"date\"> January 15th, 2015</div>      </div>      <div class=\"app_menu_item\">        <a href=\"c4-tech-issues.html\">Issues in the industry</a>        <div class=\"date\"> January 2nd, 2015</div>      </div>      <div class=\"app_menu_item\">        <a href=\"c3_thinking-style.html\">My thinking style</a>        <div class=\"date\"> Dec 25, 2014</div>      </div>     <div class=\"app_menu_item\">        <a href=\"c1-chefs-kitchen.html\">Chefs kitchen</a>        <div class=\"date\"> Dec 10, 2014</div>      </div>          <button onclick=\"display_blogs()\">Browse blogs</button>"
+
+
+
+
+
+
+}
