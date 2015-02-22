a15974197e6fed84a3f522332ed2f347fbf3fa0c
laomatt <laomatt1@gmail.com>
2015-02-21 18:46:59 -0800

    more words

--- rails_bridge/suggestotron/app/controllers/application_controller.rb
+++ rails_bridge/suggestotron/app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+# //makes a login page from the 'devise' gem
+  before_action :authenticate_user!
 end
